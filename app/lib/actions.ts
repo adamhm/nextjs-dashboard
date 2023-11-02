c1b55ce581d912c499f07c462cd7ac9a086af893
Adam Hamar <adam.hamar@gmail.com>
2023-11-02 01:23:04 +0100

    create a server action for creating invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const InvoiceSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(["pending", "paid"]),
+    date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get("customerId"),
+        amount: formData.get("amount"),
+        status: formData.get("status"),
+    });
+
+    // store monetary values in cents in your database to eliminate JavaScript
+    // floating - point errors
+    const amountInCents = amount * 100;
+
+    // create a new date with the format "YYYY-MM-DD" for the invoice's creation date
+    const date = new Date().toISOString().split("T")[0];
+
+    // insert the new invoice into the database
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    // Since you're updating the data displayed in the invoices route, you want to clear
+    // this cache and trigger a new request to the server.
+    // Once the database has been updated, the /dashboard/invoices path will be revalidated,
+    // and fresh data will be fetched from the server.
+    revalidatePath("/dashboard/invoices");
+
+    // redirect the user back to the /dashboard/invoices page
+    redirect("/dashboard/invoices");
+}
