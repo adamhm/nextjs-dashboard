dd75b1bf427d1a44eda30a332ab3fcee40119d50
Adam Hamar <adam.hamar@gmail.com>
2023-11-06 00:39:01 +0100

    add server-side validation for updating invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -79,13 +79,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get("customerId"),
         amount: formData.get("amount"),
         status: formData.get("status"),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing fields. Failed to update invoice.",
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -101,7 +113,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     revalidatePath("/dashboard/invoices");
 
     /* redirect is being called outside of the try/catch block. This is because redirect
-    works by throwing an error, which would be caught by the catch block.To avoid this,
+    works by throwing an error, which would be caught by the catch block. To avoid this,
     you can call redirect after try/catch. */
     redirect("/dashboard/invoices");
 }
